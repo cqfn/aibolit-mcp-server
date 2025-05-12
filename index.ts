69f9e6b33036f41148a34282e7cc98e9ea01c03e
Yegor Bugayenko <yegor256@gmail.com>
2025-05-12 15:50:28 +0300

    #1 typo

--- index.ts
+++ index.ts
@@ -6,8 +6,6 @@
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { server } from './src/server';
 import './src/tools';
-import './src/resources';
-import './src/prompts';
 
 (async (): Promise<void> => {
   await server.connect(new StdioServerTransport());
